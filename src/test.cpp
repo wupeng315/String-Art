bc8ce7aed3f8141f0a8f067b82e4a4fb5138048b
Ubuntu <ubuntu@ip-172-31-84-144.ec2.internal>
2019-04-10 21:04:23 +0000

    add the support for opencv library

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <opencv2/opencv.hpp>
+
 using Eigen::MatrixXd;
+using cv::Mat;
+
 int main()
 {
   MatrixXd m(2,2);
@@ -8,5 +12,5 @@ int main()
   m(1,0) = 2.5;
   m(0,1) = -1;
   m(1,1) = m(1,0) + m(0,1);
-  std::cout << m << std::endl;
+  std::cout << m  << std::endl;
 }
